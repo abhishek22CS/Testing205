fea1cf89ecb91c59430415449702a004e5e03c20
Nitin <bhadoriaabhishek@gmail.com>
2024-10-14 22:41:13 +0530

    14-10-2024

--- routes/flightenquiry.js
+++ routes/flightenquiry.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 var pool=require('./pool')
 
-/* GET home page. */
-router.get('/flightpage', function(req, res, next) {
+ router.get('/flightpage', function(req, res, next) {
 
-  res.render('flightinterface', { title: 'a' });
+  res.render('flightinterface', {message: '' });
 });
 
 router.get('/flightsubmit',function(req,res){
+  // console.log("days",req.query.days)
+  var days=(""+req.query.days).replaceAll("'",'"')
   pool.query("insert into flightdetails (flightname, types, totalseats, days, sourcecity, departuretime, destinationcity, arrivaltime, company, logo ) values(?,?,?,?,?,?,?,?,?,?)",
     [req.query.flightname,
       req.query.flighttype,
       req.query.noofseat,
-      req.query.days,
+       days,
       req.query.sourcecity,
       req.query.deptime,
       req.query.destinationcity,
@@ -25,7 +26,7 @@ router.get('/flightsubmit',function(req,res){
     {
     if(error)
     {
-      // console.log(error)
+      console.log(error)
       res.render("flightinterface",{'message':'Server Error'})
     }
     else{
@@ -34,5 +35,21 @@ router.get('/flightsubmit',function(req,res){
   }
   )
 })
+// ---------------------------------------------------------------
+// cities api
+
+router.get('/fetchallcities', function(req, res, next) {
+pool.query("select * from cities ",function(error,result){
+  if(error)
+  {
+    res.status(500).json({result:[],message:'server Error'})
+  }
+  else{
+    res.status(200).json({result:result,message:'Success'})
+
+  }
+})
+ });
+
 
 module.exports = router;
